8d43ed4526a88821baed125f83ab79c1c75e7e19
Brett Carr <brett@nawundi.org>
2019-08-07 11:25:51 +1000

    Update to BigDecimal handling

--- src/test/FormulaEvaluatorTest.java
+++ src/test/FormulaEvaluatorTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,13 +10,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FormulaEvaluatorTest {
 
+  MathContext mathContext = new MathContext(2, RoundingMode.HALF_DOWN);
+
   @Test
   public void evaluateFormulaWithAdditionMultiplication() {
     Map<String, Double> variables = new HashMap<>();
     variables.put("x", 1d);
     variables.put("y", 2d);
 
-    assertEquals(new BigDecimal(30.00).setScale(2), FormulaEvaluator.evaluate("({x}+{y})*10", variables));
+    assertEquals(new BigDecimal(30.00, mathContext).setScale(2), FormulaEvaluator.evaluate("({x}+{y})*10", variables));
 
   }
 
@@ -24,7 +28,8 @@ class FormulaEvaluatorTest {
     variables.put("x", .3d);
     variables.put("n", 10d);
 
-    assertEquals(new BigDecimal(3.30).setScale(2), FormulaEvaluator.evaluate("({x}*{n})+{x}", variables));
+
+    assertEquals(new BigDecimal(3.30d, mathContext).setScale(2), FormulaEvaluator.evaluate("({x}*{n})+{x}", variables));
 
   }
 }
